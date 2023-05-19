d27a4572165b30066ce1b02359fb9c82e86f980c
swj2023 <qkedkfkvkd@gmail.com>
2023-05-19 16:59:18 +0900

    PC화면 부트스트랩 제거 PC화면 및 모바일 화면 CSS 완성

--- js/cocktail_certificate.js
+++ js/cocktail_certificate.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+document.onload = function() {
     // 컴퓨터를 켰는데 굳이 모바일 화면으로 변경해서 볼 이유가 없으므로..
     // 처음 화면이 로드될 때 화면의 크기를 판단하여 PC화면으로 보이게 할지,
     // 모바일 화면으로 보이게할 지 결정한다.
@@ -6,27 +6,14 @@ window.onload = function() {
     if(mql.matches) {
         document.getElementById('pc_screen').style.display = 'none';
         document.getElementById('mobile_screen').style.display = 'block';
-        test3Content();
+        cocktail_certificate_exam3();
     }
 };
 
-// 반응형 웹으로 구현
-window.onresize = function() {
-    var mql = window.matchMedia("screen and (max-width:480px)");
-    if(mql.matches) {
-        document.getElementById('pc_screen').style.display = 'none';
-        document.getElementById('mobile_screen').style.display = 'block';
-        test3Content();
-    } else {
-        document.getElementById('pc_screen').style.display = 'block';
-        document.getElementById('mobile_screen').style.display = 'none';
-    }
-}
-
 
 // 1차 시험 내용
-document.getElementById('test').addEventListener('click', function(){
-    this.classList.add('active');
+function cocktail_certificate_exam1() {
+    document.getElementById('test1').classList.add('active');
     document.getElementById('test2').classList.remove('active');
     document.getElementById('test3').classList.remove('active');
     document.getElementById('test4').classList.remove('active');
@@ -41,12 +28,13 @@ document.getElementById('test').addEventListener('click', function(){
 
     xhr.open('GET', './php/cocktail_certificate_exam1.php', true);
     xhr.send();
-});
+}
+
 
 // 2차 시험 내용
-document.getElementById('test2').addEventListener('click', function(){
-    this.classList.add('active');
+function cocktail_certificate_exam2() {
     document.getElementById('test1').classList.remove('active');
+    document.getElementById('test2').classList.add('active');
     document.getElementById('test3').classList.remove('active');
     document.getElementById('test4').classList.remove('active');
 
@@ -60,13 +48,14 @@ document.getElementById('test2').addEventListener('click', function(){
     
     xhr.open('GET', './php/cocktail_certificate_exam2.php', true);
     xhr.send();
-});
+}
+
 
 // 3차 시험 내용
-document.getElementById('test3').addEventListener('click', function(){
-    this.classList.add('active');
+function cocktail_certificate_exam3() {
     document.getElementById('test1').classList.remove('active');
     document.getElementById('test2').classList.remove('active');
+    document.getElementById('test3').classList.add('active');
     document.getElementById('test4').classList.remove('active');
     
     var url = "./php/cocktail_certificate_exam3.php";
@@ -74,18 +63,33 @@ document.getElementById('test3').addEventListener('click', function(){
     .then(function(responseText){
         document.getElementById('test_content').innerHTML = responseText;
     });
-});
+}
+
 
 // 4차 시험 내용
-document.getElementById('test4').addEventListener('click', function(){
-    this.classList.add('active');
+function cocktail_certificate_exam4() {
     document.getElementById('test1').classList.remove('active');
     document.getElementById('test2').classList.remove('active');
     document.getElementById('test3').classList.remove('active');
+    document.getElementById('test4').classList.add('active');
 
     fetch("./php/cocktail_certificate_exam4.php")
     .then(function(response){return response.text()})
     .then(function(responseText){
         document.getElementById('test_content').innerHTML = responseText;
     });
-});
+}
+
+
+// 반응형 웹으로 구현
+window.onresize = function() {
+    var mql = window.matchMedia("screen and (max-width:480px)");
+    if(mql.matches) {
+        document.getElementById('pc_screen').style.display = 'none';
+        document.getElementById('mobile_screen').style.display = 'block';
+        cocktail_certificate_exam3();
+    } else {
+        document.getElementById('pc_screen').style.display = 'block';
+        document.getElementById('mobile_screen').style.display = 'none';
+    }
+}
