f903876b0368a329d09b02fce74b105555a32cb9
incava <68988975+incava@users.noreply.github.com>
2023-05-22 14:58:08 +0900

    feat : 인덱스와 사진크기가 다르도록 변경

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 function loadHTML(id) {
     let url = '../php/load_menu.php?id=' + id;
-
     fetch(url)
         .then(response => response.text())
         .then(html => {
@@ -12,11 +11,25 @@ function loadHTML(id) {
                 script.src = s.src
                 document.head.appendChild(script)
             })
+            menuNone();
         })
         .catch(error => {
             console.error('Error:', error);
         });
 
 }
+
+
+function menuAppear() {
+    divElement.classList.replace('text-pageheader','text-homeimage')
+}
+function menuNone() {
+    divElement.classList.replace('text-homeimage','text-pageheader')
+}
+//menu div위치를 전역변수로 만들기 위한 함수.
+function defineMenu(){
+    divElement = document.querySelector('.text-homeimage');
+}
+
 
 
