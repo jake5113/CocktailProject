63a0035376a0975344473d43fb761fd11985df62
swj2023 <qkedkfkvkd@gmail.com>
2023-05-22 13:26:23 +0900

    모바일 화면 로드 후 시험 내용 보이게 수정

--- js/cocktail_certificate.js
+++ js/cocktail_certificate.js
@@ -1,14 +1,12 @@
-document.onload = function() {
-    // 컴퓨터를 켰는데 굳이 모바일 화면으로 변경해서 볼 이유가 없으므로..
-    // 처음 화면이 로드될 때 화면의 크기를 판단하여 PC화면으로 보이게 할지,
-    // 모바일 화면으로 보이게할 지 결정한다.
-    var mql = window.matchMedia("screen and (max-width:480px)");
-    if(mql.matches) {
-        document.getElementById('pc_screen').style.display = 'none';
-        document.getElementById('mobile_screen').style.display = 'block';
-        cocktail_certificate_exam3();
-    }
-};
+// 컴퓨터를 켰는데 굳이 모바일 화면으로 변경해서 볼 이유가 없으므로..
+// 처음 화면이 로드될 때 화면의 크기를 판단하여 PC화면으로 보이게 할지,
+// 모바일 화면으로 보이게할 지 결정한다.
+var mql = window.matchMedia("screen and (max-width:480px)");
+if(mql.matches) {
+    document.getElementById('pc_screen').style.display = 'none';
+    document.getElementById('mobile_screen').style.display = 'block';
+    cocktail_certificate_exam3();
+}
 
 
 // 1차 시험 내용
